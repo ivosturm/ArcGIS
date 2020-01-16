28c8b8a5de6ee4561da2efea148c72190a6e1627
Demonie <koen.demonie@capgemini.com>
2020-01-16 13:35:29 +0100

    Added zoomToFitDeclarations

--- src/widget/ArcGIS.js
+++ src/widget/ArcGIS.js
@@ -435,8 +435,9 @@ require(dojoConfig, [], function() {
 				// if no map available, so at initial load after postCreate, load map in callback of getHostName
 				if (!this._gisMap) {
 					this._getHostNameMF(callback);
-					if (this.customZoomlevel) {this._getCustomZoomLevelMF(); }
-					else{
+					if (this.customZoomlevel) {
+						this._getCustomZoomLevelMF();
+					} else {
 						this.zoomlevel = this.defaultZoom;
 					}
 				} // if map  already available, zoom to object if available. use coordinates if existing, else query ArcGIS to get coordinates
@@ -2331,10 +2332,13 @@ require(dojoConfig, [], function() {
 			},
 			_getDeclarationColorForStatus(status) {
 				const fallbackColor = "#1e6b00";
-				if(!status){
-					return fallbackColor;;
+				if (!status) {
+					return fallbackColor;
 				}
-				const statusColor = this.statusColorList.find( statusColorObj => statusColorObj.statusLabel.toUpperCase() === status.toUpperCase() );
+				const statusColor = this.statusColorList.find(
+					statusColorObj =>
+						statusColorObj.statusLabel.toUpperCase() === status.toUpperCase()
+				);
 
 				return statusColor ? statusColor.statusColor : fallbackColor;
 			},
@@ -2358,9 +2362,9 @@ require(dojoConfig, [], function() {
 					);
 				}
 			},
-			_execNewReportChangekMf: function(){
+			_execNewReportChangekMf: function() {
 				const guid = this._contextObj.getGuid();
-				if(guid){
+				if (guid) {
 					mx.data.action(
 						{
 							params: {
@@ -2389,8 +2393,7 @@ require(dojoConfig, [], function() {
 						Number(latitude).toFixed(8)
 					);
 
-				this.onNewReportChangeMF &&
-					this._execNewReportChangekMf();
+				this.onNewReportChangeMF && this._execNewReportChangekMf();
 			},
 			_getDeclarationsData: function() {
 				if (this.getReportsMF) {
@@ -2409,6 +2412,13 @@ require(dojoConfig, [], function() {
 								);
 								console.dir(this._logNode + objs);
 							}
+
+							if (this.zoomToFit) {
+								this._zoomToFitDeclarations(objs);
+							} else {
+								this.zoomFitLevel = undefined;
+							}
+
 							this._createExistingDeclarationsLayer(objs);
 						}),
 						error: lang.hitch(this, function(error) {
@@ -2417,6 +2427,32 @@ require(dojoConfig, [], function() {
 					});
 					// if no other objects need to be loaded, just load the map, assuming the context entity is the object to load
 				}
+			},
+			_zoomToFitDeclarations: function(declarationMFResult) {
+				const points = declarationMFResult.map(declaration => {
+					const attributes = declaration.jsonData.attributes;
+
+					return new esri.geometry.Point({
+						x: attributes[this.DeclarationLongitude].value,
+						y: attributes[this.DeclarationLatitude].value,
+						spatialReference: new esri.SpatialReference({
+							wkid: Number(this.spatialReference)
+						})
+					});
+				});
+
+				this.geometryService.convexHull(points,
+					/*callback=*/geometry => {
+						this.declarationsExtent = geometry.getExtent();
+						this._gisMap.setExtent(this.declarationsExtent);
+					},
+					/*errback=*/ err => {
+						if (this.consoleLogging) {
+							console.log("Something went wrong during the convex hull calculation of the reports.");
+							console.log(JSON.stringify(err));
+						}
+					}
+				);
 			}
 		});
 	});
